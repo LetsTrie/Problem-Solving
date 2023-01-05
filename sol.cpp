d477535425cbc79ea7d867df05f2d1a56d3826f0
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-05 23:51:23 +0600

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -165,10 +165,9 @@ LL calculateLcm(LL n, LL m) {
 // ***************************************************************************************************
 // ***************************************************************************************************
 
-// String DS
-// Ascii code: [A-Z, 65-90], [a-z,  97-122], [0-9, 48-57]
+// String DS: Trie
+// 1. https://leetcode.com/problems/longest-common-prefix/
 
-// Trie 
 int nNodes = 1, nString = 0, ROOT = 0, nLetters = 122;
 
 vector<vector<int>> edge(MAXN + 1, vector<int>(nLetters + 1));
@@ -222,11 +221,11 @@ string findLongestCommonPrefix(int node = ROOT, int id = 0) {
   return "";
 }
 
-
 // ***************************************************************************************************
 // ***************************************************************************************************
 
 // Miscellaneous
-/*
-*  Middle of 1 to N is: if (N & 1) then (N + 1) / 2 else (N / 2) or (N / 2) + 1
-*/
+// 1. Middle of 1 to N is: if (N & 1) then (N + 1) / 2 else (N / 2) or (N / 2) + 1
+// 2. ASCII code: [A-Z, 65-90], [a-z,  97-122], [0-9, 48-57]
+
+
