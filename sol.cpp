ff7200f172a449563c5604d5ecf478aba1e3977e
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-07 02:52:17 +0600

    Trie with linked list

--- sol.cpp
+++ sol.cpp
@@ -165,11 +165,79 @@ LL calculateLcm(LL n, LL m) {
 // ***************************************************************************************************
 // ***************************************************************************************************
 
-// String DS: Trie [Non Linked-List Solution]
+// String DS: Trie
 // 1. https://leetcode.com/problems/longest-common-prefix/
 // 2. https://leetcode.com/problems/lexicographical-numbers/
 // 3. https://leetcode.com/problems/implement-trie-prefix-tree/
+// 4. https://leetcode.com/problems/search-suggestions-system/
 
+// Trie [Linked-List Solution]
+// Change nLetters/strToASCII according to problem!
+
+const int nLetters = 26;
+
+int strToASCII (char ch) { return ch - 'a'; }
+char ASCIItoStr(int ch) { return ch + 'a'; }
+
+class TrieNode {
+  public:
+    TrieNode *next[nLetters + 1] = { nullptr };
+    int serial = 0;
+    int prefixCount = 0;
+    bool isEndOne = false;
+
+    TrieNode(int _x = 0) : serial(_x) {}
+};
+
+class Trie {
+    TrieNode *root;
+    int nString = 0;
+    int nNode = 1;
+    vector<string> lexicoStrings;
+
+public:
+    Trie() { root = new TrieNode(); }
+    
+    void insert(string word) {
+      TrieNode *p = root;
+
+      for(char &ch: word) {
+        int val = strToASCII(ch);
+        if(p->next[val] == NULL) p->next[val] = new TrieNode(nNode++);
+        p = p->next[val];
+        p->prefixCount++;
+      }
+      nString++;
+      p->isEndOne=true;
+    }
+   void solve () {
+    // write your code here...
+   }
+
+  private:
+    TrieNode* _findByKey(string key) {
+      TrieNode *node = root;
+      for(char &ch: key) {
+        int value = strToASCII(ch);
+        node = node->next[value];
+        if(node == NULL) break;
+      }
+      return node;
+    }
+
+    void _getLexicalOrder(TrieNode *node, string &cur, vector<string> &res) {
+      if(node->isEndOne == true) res.push_back(cur);
+      for(int i = 0; i < nLetters; i++) {
+        if(node->next[i] != NULL) {
+          cur += ASCIItoStr(i);
+          _getLexicalOrder(node->next[i], cur, res);
+          cur.pop_back();
+        }
+      }
+    }
+};
+
+// Trie [Non Linked-List Solution]
 int nNodes = 1, nString = 0, ROOT = 0, nLetters = 122;
 
 vector<vector<int>> edge(MAXN + 1, vector<int>(nLetters + 1));
@@ -221,12 +289,5 @@ void clearTrie() {
 // 1. Middle of 1 to N is: if (N & 1) then (N + 1) / 2 else (N / 2) or (N / 2) + 1
 // 2. ASCII code: [A-Z, 65-90], [a-z,  97-122], [0-9, 48-57]
 
-string intToStr (int n) {
-  stringstream ss; ss << n; 
-  string str; ss >> str;
-  return str;
-}
-
-int strToInt (string str) {
-  return stoi(str);
-}
+string intToStr (int n) { stringstream ss; ss << n; string str; ss >> str; return str; }
+int strToInt (string str) { return stoi(str); }
