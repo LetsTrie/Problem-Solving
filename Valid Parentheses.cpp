19141b43292625020218f540349fef7063e17fd0
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-09 01:31:21 +0600

    Create Valid Parentheses.cpp

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,18 @@
+// Use-of: Stack 
+// Problem 1. https://leetcode.com/problems/valid-parentheses/
+
+bool isValid(string s) {
+  stack <char> parenthesis;
+  for (auto const & ch: s) {
+    if (ch == '(' || ch == '{' || ch == '[') parenthesis.push(ch);
+    else {
+      if (parenthesis.empty()) return false;
+      else if (ch == ')' && parenthesis.top() == '(') parenthesis.pop();
+      else if (ch == '}' && parenthesis.top() == '{') parenthesis.pop();
+      else if (ch == ']' && parenthesis.top() == '[') parenthesis.pop();
+      else return false;
+    }
+  }
+
+  return parenthesis.empty();
+}
