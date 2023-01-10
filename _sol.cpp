e2fd01daa9bba3bea8158073d168af1028297fd1
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-10 21:07:26 +0600

    N decimal places

--- _sol.cpp
+++ _sol.cpp
@@ -23,7 +23,21 @@ int main() {
 	// Write code here...
 }
 
+// N decimal places
+// cout << fixed << setprecision(6);
+// cout << (1.0 * 2 / 3) << endl;
+
 // vector <int> dp(MAXN + 1, -1);
+
 // vector<vector<LL>> nCr(N, vector<LL> (M, -1));
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
 // SegmentTree* segmentTree = new SegmentTree(vec, N);
 // segmentTree -> build(vec, 1, 0, N - 1);
