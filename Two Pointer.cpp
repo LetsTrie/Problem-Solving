4224b68418156b20f597fe7abfa512b75a7297e1
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-11 00:29:11 +0600

    New Problem

--- Two Pointer.cpp
+++ Two Pointer.cpp
@@ -1,1 +1,2 @@
-// https://leetcode.com/problems/remove-nth-node-from-end-of-list/solutions/1164542/js-python-java-c-easy-two-pointer-solution-w-explanation/?orderBy=most_votes
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/solutions/1164542/js-python-java-c-easy-two-pointer-solution-w-explanation/
+// Problem: https://www.hackerrank.com/challenges/one-month-preparation-kit-the-birthday-bar/problem
