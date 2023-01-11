cdc67a9a3f8cd1871c8b667e96609c9fd4223422
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-12 02:40:02 +0600

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -29,11 +29,3 @@ LL _lcm(LL n, LL m) {
 // Formula: nc1+nc2+nc3+nc4+...+ncn = (2^n)-1
 // Formula: 1+2+3+...+n = n(n+1)/2
 // Problem: https://leetcode.com/problems/number-of-substrings-with-only-1s/
-
-// x ^ x = 0, x ^ x ^ x = x 
-// Problem: https://www.hackerrank.com/challenges/one-month-preparation-kit-lonely-integer/problem
-
-// Number of bits in one integer = log2(num) + 1
-// Flip one bit: num ^= (1 << i); // flip i'th bit
-// Flip all bits in 32 bit number: for(int i = 0; i < 32; i++) num ^= (1 << i)
-// Problem: https://www.hackerrank.com/challenges/one-month-preparation-kit-flipping-bits/problem
