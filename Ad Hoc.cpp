a3f8d8d56d3971631b8864805b637f72036f0b2f
MD. Sakib Khan <sakibkhan111296@gmail.com>
2023-01-12 00:16:42 +0600

    Create Ad Hoc.cpp

---
+++ Ad Hoc.cpp
@@ -0,0 +1,2 @@
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-drawing-book/problem/  
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-angry-children/problem/
