ccdd8d64b3946aafea629e6981fb42456f53b4c4
MD. Sakib Khan <2016-314-436@student.cse.du.ac.bd>
2023-01-31 17:05:14 +0600

    Segment Tree

--- Segment Tree.cpp
+++ Segment Tree.cpp
@@ -1,59 +1,97 @@
-// Segment tree (+Lazy propagation)
-// Problem 1: https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/A
-
-class SegmentTree {
-  vector<LL> tree;
-  vector<LL> lazy;
-
-public:
-	SegmentTree() {}
-	SegmentTree(vector<int>& vec, int _length) {
-	   tree.resize(_length << 2, 0);
-	   lazy.resize(_length << 2, 0);
-	   build(vec, 1, 0, _length - 1);
-	}
+typedef long long LL;
  
-	void build(vector<int>& vec, int pos, int left, int right) {
-	  if(left == right) {
-	  	tree[pos] = vec[left], lazy[pos] = 0; 
-	  	return ;
-	  }
-	  int mid = (left + right) / 2, l = pos << 1, r = l | 1;
-	  build(vec, l, left , mid), build(vec, r, mid + 1 , right);
-	  tree[pos] = tree[l] + tree[r], lazy[pos] = lazy[l] + lazy[r];
-	}
+// SegTree* segmentTree = new SegTree();
+class SegTree {
+public:
+    LL sum, max_value, lazy;
+    SegTree *left_node, *right_node;
+    
+    SegTree(){
+        sum = 0;
+        lazy = 0;
+        max_value = INT_MIN;
+        left_node = nullptr;
+        right_node = nullptr;
+    }
+    
+    // segmentTree -> build(vec, 0, n - 1); 
+    void build(vector<int>& vec, int left, int right) {
+      if(left == right) {
+          this -> sum       =   vec[left];
+          this -> max_value =   vec[left];
+          this -> lazy      =   0; 
+          return;
+      }
+      
+      left_node = new SegTree();
+      right_node = new SegTree();
+      
+      int mid = (left + right) / 2;
+      build(vec, left , mid);
+      build(vec, mid + 1 , right);
+      
+      left_node -> build(vec, left , mid);
+      right_node -> build(vec, mid + 1 , right);
+      
+      this -> sum       =   (left_node -> sum) + (right_node -> sum); 
+      this -> max_value =   max(left_node -> max_value, right_node -> max_value); 
+      this -> lazy      =   0; 
+    }
 
-	void update(int pos, int left, int right, int tl, int tr, int value) {
-	  int mid = (left + right) / 2, l = pos << 1, r = l | 1;
-	  if(lazy[pos]) {
-	    tree[pos] = (LL)lazy[pos] * (right - left + 1);
-	    if(left != right) lazy[l] = lazy[pos] , lazy[r] = lazy[pos];
-	    lazy[pos] = 0; 
-	  } 
-	  if(left > right or left > tr or right < tl) return;
-	  if(tl <= left and tr >= right) {
-	    tree[pos] = (LL)value * (right - left + 1);
-	    if(left != right) lazy[l] = value , lazy[r] = value; 
-	    return;
-	  } 
-	  update(l, left, mid, tl, tr, value), update(r, mid + 1, right, tl, tr, value);
-	  tree[pos] = tree[l] + tree[r];
-	}
+    // segmentTree-> update(0, n - 1, tl, tr, v);
+    void update(int left, int right, int tl, int tr, int value) {
+      int mid = (left + right) / 2;
 
-	LL get(int pos, int left, int right, int tl, int tr) {
-	  int mid = (left + right) / 2, l = pos << 1, r = l | 1;
-	  if(lazy[pos]) {
-	    tree[pos] = (LL)lazy[pos] * (right - left + 1);
-	    if(left != right) lazy[l] = lazy[pos], lazy[r] = lazy[pos];
-	    lazy[pos] = 0; 
-	  } 
-	  if(left > right or left > tr or right < tl) return 0LL;
-	  if(tl <= left and tr >= right) return tree[pos];
-	  return get(l, left, mid, tl, tr) + get(r, mid + 1, right, tl, tr);
-	}
+      if(this -> lazy) {
+        this -> max_value   += this -> lazy;
+        this -> sum         += (LL) this -> lazy * (right - left + 1);
+        if(left != right) {
+            left_node   -> lazy +=  this -> lazy;
+            right_node  -> lazy +=  this -> lazy;
+        }
+        this -> lazy = 0; 
+      } 
+      
+      if(left > right or left > tr or right < tl) return;
+      
+      
+      if(tl <= left and tr >= right) {
+        this -> max_value   += value;
+        this -> sum         += (LL)value * (right - left + 1);
+        if(left != right) {
+            left_node   -> lazy +=  value;
+            right_node  -> lazy +=  value;
+        }
+        return;
+      } 
+      
+      left_node 	-> update(left, mid, tl, tr, value);
+      right_node 	-> update(mid + 1, right, tl, tr, value);
+      
+      this -> sum       =   (left_node -> sum) + (right_node -> sum); 
+      this -> max_value =   max(left_node -> max_value, right_node -> max_value); 
+    }
+
+    // segmentTree-> get(0, n - 1, 0, n - 1)
+    LL get(int left, int right, int tl, int tr) {
+      int mid = (left + right) / 2;
+
+      if(this -> lazy) {
+        this -> max_value   += this -> lazy;
+        this -> sum         += (LL) this -> lazy * (right - left + 1);
+        if(left != right) {
+            left_node   -> lazy +=  this -> lazy;
+            right_node  -> lazy +=  this -> lazy;
+        }
+        this -> lazy = 0; 
+      } 
+      
+      if(left > right or left > tr or right < tl) return 0LL;
+      if(tl <= left and tr >= right) return this -> max_value;
+
+      int left_result = left_node -> get(left, mid, tl, tr);
+      int right_result = right_node -> get(mid + 1, right, tl, tr);
+      
+      return max(left_result, right_result);
+    }
 };
-
-// SegmentTree* segmentTree = new SegmentTree(vec, N);
-// segmentTree -> build(vec, 1, 0, N - 1);
-// segmentTree -> update(1, 0, N - 1, tl, tl, tr);
-// segmentTree -> get(1, 0, N - 1, tl, tr - 1);
